14861281ba5f61bce864b73adcbd4bec8ec80fd8
ridonk <georgeddunbar@gmail.com>
2018-12-29 11:16:10 -0500

    Fixed minor typo in Update script and changed default text of conky to red

--- Update.sh
+++ Update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # A simple script to update debian based systems
 sudo apt update
-sudo apt -y dist-ugprade
+sudo apt -y dist-upgrade
 sudo apt -y auto-remove
 sudo apt clean
 sudo apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
